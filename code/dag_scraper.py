ba1dd33658e6dec697b905fa9d4a631971a23986
Wagner Fernández <wagnerfernandez17@gmail.com>
2023-09-04 09:51:54 -0500

    add code comments

--- code/dag_scraper.py
+++ code/dag_scraper.py
@@ -1,20 +1,22 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+#1- Importar las librerías necesarias para crear la tubería de datos que extraerá la información y exportarla a un documento delimitado por comas (.csv)
+from datetime import datetime, timedelta # librería que integra Apache Airflow para la asignar tiempo para la ejecución de la tubiería de datos
+from airflow import DAG # librería que integra con la API de Airflow y orquestar las tuberias de datos
+from airflow.operators.python_operator import PythonOperator # orquestador de tareas para ejecutar tareas con codigo python
 
+#2- Crear 
 def scrape_and_save_data():
-    import os
+    import os #  para la gestión de parametros en el sistema como la ejecución adecuada de las rutas
     import sys
-    import pandas as pd
-    import requests
-    from bs4 import BeautifulSoup
+    import pandas as pd # para la manipulación y limpieza de datos
+    import requests # hacer peticiones cliente servidor para poder hacer la extracción de datos
+    from bs4 import BeautifulSoup # para la extracción de datos a partir de documentos en HTML
 
     # Código para scraping y procesamiento de datos
-    url = "https://www.larepublica.co/"
+    url = "https://www.larepublica.co/" # pagina de la cual se va aextraer la información
     respuesta = requests.get(url)
-    print("La respuesta del servidor es:", respuesta)
+    print("La respuesta del servidor es:", respuesta) # en la librería beautifulsoup si da 200, es porque la extracción es exitosa
 
-    path = '/home/wagner/Documents/PROYECTOS DATASCIENCE/PROYECTOS PYTHON/WORKFLOW-DAG-AIRFLOW/data/scraped.txt'
+    path = '/home/wagner/Documents/PROYECTOS DATASCIENCE/PROYECTOS PYTHON/WORKFLOW-DAG-AIRFLOW/data/scraped.txt' # ubicación en el nodo donde se va a almacenar los datos raspados
 
     with open(path + 'extraccion.txt', 'wb') as output:
         output.write(respuesta.content)
