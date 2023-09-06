a0aa08e68c71ad8051e0e2c5c9c5fdd2d98ab64e
Wagner Fernández <wagnerfernandez17@gmail.com>
2023-09-06 17:22:02 -0500

    Comments updated

--- code/load_to_dw.py
+++ code/load_to_dw.py
@@ -1,21 +1,22 @@
 #Importar las librerías necesarias para crear el DAG
-from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-import pandas as pd
-import os
+from airflow import DAG # # librería que integra con la API de Airflow y orquestar las tuberias de datos
+from airflow.providers.postgres.operators.postgres import PostgresOperator # Librería para que funcione insertar los datos desde la ubicación  
+from airflow.operators.python import PythonOperator # orquestador de tareas para ejecutar tareas con codigo python
+from datetime import datetime # para configurar y poner en marcha la fecha y hora 
+import pandas as pd # para la manipulación y limpieza de datos
+import os #  para la gestión de parametros en el sistema como la ejecución adecuada de las rutas para acceder a carpetas y archivos
 
-# Definir los parámetros del DAG
+# Definir los parámetros del DAG en cuanto a fecha de inicio y veces en que se ejecuta
 default_args = {
-    'owner': 'wagner',
-    'start_date': datetime(2023, 9, 3),
+    'owner': 'wagner', # etiqueta del DAG 
+    'start_date': datetime(2023, 9, 3), # fecha de inicio del DAG
     'depends_on_past': False,
-    'retries': 1,
+    'retries': 1, # numero de veces en las que se ejecuta el DAG en Airflow
 }
 
+#Nombre al que se le asigna al DAG
 dag = DAG(
-    'cargar_csv_links_a_postgresql',
+    'cargar_csv_links_a_postgresql', # nombre del DAG
     default_args=default_args,
     schedule_interval=None,  # Aqui se puede configurar el desencadenamiento según las necesidades,en este caso se puede hacer manual o automaticamente
     catchup=False,
@@ -42,9 +43,9 @@ def cargar_csv_links_a_postgresql():
         raise FileNotFoundError(f'El archivo CSV {csv_file_path} no existe.')# en caso de que no exista el archivo 
 
 cargar_csv_task = PythonOperator(
-    task_id='cargar_csv_links_a_postgresql',
-    python_callable=cargar_csv_links_a_postgresql,
-    dag=dag,
+    task_id='cargar_csv_links_a_postgresql', # nombre de la tarea
+    python_callable=cargar_csv_links_a_postgresql, # por parte del operador Python, se llama la función o nombre del DAG
+    dag=dag, # instancia  que llama al DAG
 )
 
 if __name__ == "__main__":
